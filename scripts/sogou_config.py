69ae72ea2ec9ff8b93fe7cb647d692d7f37addb1
raawaa <wenjie_yu@yeah.net>
2021-11-30 17:27:08 +0800

    保留所有emoji
    原程序对于重码的emoji只保留开头两个。本版本保留所有emoji

--- scripts/sogou_config.py
+++ scripts/sogou_config.py
@@ -7,6 +7,5 @@ def write_config(data: str, output: str):
     with open(output, 'w', encoding="utf-8") as o:
         for k in raw_data:
             emojis = raw_data[k]
-            o.write(f"{k},2={emojis[0]}\n")
-            if len(emojis) > 1:
-                o.write(f"{k},3={emojis[1]}\n")
+            for i in range(len(emojis)):
+                o.write(f"{k},{i+3}={emojis[i]}\n")
